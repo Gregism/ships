c51a93801722da8bff2317be04424767c237555e
Gregory Smith <gregory.terrell.smith@gmail.com>
2015-06-28 16:33:56 -0500

    Groups and more

--- main.js
+++ main.js
@@ -1,19 +1,78 @@
-var map = L.map('map').setView([51.505, -0.09], 2);
+var map = L.map('map')
+if(window.location.href.indexOf('?') > -1){
+  getHistory(null, window.location.search.split('ship-input=')[1]);
+ // map.setView([0, 0], 3);
+}else{
+  map.setView([18.45, -66], 5);
+}
+
 L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ3JlZ2lzbSIsImEiOiJiNjMwMWE5MWUwOThjOGUxYjIxMzk5Njk3ODBkM2ZjZiJ9.WjgpibCLDnpbg3vjS0MLKw',{
 
 }).addTo(map);
 
-d3.json("data/regions.json")
+$('form').on('submit', function(e){
+  e.preventDefault();
+})
+
+$('#ship-input-btn').on('click', getHistory);
+
+function getHistory(e, shipInput){
+  if($('#ship-input').val().toLowerCase() === "all"){
+      d3.json("Ships.aspx")
+        .get(function(error, rows){makeShips(rows);});
+     return false;
+  }
+  
+  $.ajax({
+    url: 'HistPos.aspx',
+    data: {"imo": shipInput || $('#ship-input').val()},
+    success: function (result){
+     makeLines(result);
+    },
+    error: function(error){
+      console.log(error);
+    },
+    datatype: 'json'
+  });
+}
+
+var progress = document.getElementById('progress');
+var progressBar = document.getElementById('progress-bar');
+
+function updateProgressBar(processed, total, elapsed, layersArray) {
+  if (elapsed > 1000) {
+    // if it takes more than a second to load, display the progress bar:
+    progress.style.display = 'block';
+    progressBar.style.width = Math.round(processed/total*100) + '%';
+  }
+
+  if (processed === total) {
+    // all markers processed - hide the progress bar:
+    progress.style.display = 'none';
+  }
+}
+
+var markers = L.markerClusterGroup({ chunkedLoading: true, chunkProgress: updateProgressBar });
+var historyMarkers = L.featureGroup([]);
+var historyLines = L.featureGroup([]);
+
+// $.ajax({
+//   url: 'ShipList.aspx',
+//   success: function (result){console.log(result)},
+//   datatype: 'json'
+// });
+
+d3.json("Regions.aspx")
   //.row(function(d){return d;})
   .get(function(error, rows){makeRegions(rows);});
 
-d3.json("data/ports.json")
+d3.json("Ports.aspx")
   //.row(function(d){return d;})
   .get(function(error, rows){ makePorts(rows); });
 
-d3.json("data/all_ships.json")
-  //.row(function(d){return d;})
-  .get(function(error, rows){makeShips(rows);});
+// d3.json("Ships.aspx")
+//   //.row(function(d){return d;})
+//   .get(function(error, rows){makeShips(rows);});
 
 ladenStatus = {"Unladen": "a00", "Laden": "0a0"};
 
@@ -41,64 +100,76 @@ L.RotatedMarker = L.Marker.extend({
   };
 
 function makeShips(rows){
+  var markerList = [];
+  removeMarkers();
+  
   $.each(rows, function(i, ship){
-    console.log(ship);
-    var coord = [+ship.Position.Longitude, +ship.Position.Latitude];
-    var icon = L.MakiMarkers.icon({icon: "marker", color: (function(){
-        if(ship.LadenStatus){
-          return '#'+ladenStatus[ship.LadenStatus]
-        }
-        return '#00b'
-      })(), size: "s"});
-      L.rotatedMarker([coord[1],coord[0]], {
-        icon: icon,
-        angle: ship.Heading+180 || 180
-      }).addTo(map);
+    var title = ship.ShipName;
+    var marker = L.marker(L.latLng(ship.Position.Latitude, ship.Position.Longitude), { title: title });
+    //marker.valueOf()._icon.style.backgroundColor = '#' + ladenStatus[ship.LadenStatus];
+    marker.bindPopup(title);
+    markerList.push(marker);
+
+    // var coord = [+ship.Position.Longitude, +ship.Position.Latitude];
+    // var icon = L.MakiMarkers.icon({icon: "marker", color: (function(){
+    //     if(ship.LadenStatus){
+    //       return '#'+ladenStatus[ship.LadenStatus]
+    //     }
+    //     return '#00b'
+    //   })(), size: "s"});
+    //   L.rotatedMarker([coord[1],coord[0]], {
+    //     icon: icon,
+    //     angle: ship.Heading+180 || 180
+    //   }).addTo(map);
   });
+
+  markers.addLayers(markerList);
+  map.addLayer(markers);
 }
 
-function makeLines(rows){
-  var ships = _.uniq(_.pluck(rows, 'ShipName'));
+function removeMarkers(){
+  markers.clearLayers();
+  historyMarkers.clearLayers();
+  historyLines.clearLayers();
+}
 
-  $.each(ships, function(i, ship){
-    var coords = [];
-    ships[i] = {'name': ship, 'data': _.where(rows, {ShipName:ship})};
-    $.each(ships[i].data, function(i, obj){
-      coords.push([+obj.Longitude, +obj.Latitude]);
-    });
-    ships[i].coordinates = coords;
-    // L.geoJson(lineString(coords),{
-    //   "style": {
-    //      "color": "orange",
-    //     "weight": 2,
-    //   },
-    // }).addTo(map);
-
+function makeLines(ship){
+  var coords = [];
+  var markerList = [];
+  
+  removeMarkers();
+  $.each(ship.Positions, function(i, obj){
+    coords.push([+obj.Longitude, +obj.Latitude]);
+  });
+   historyLines = L.geoJson(lineString(coords),{
+      "style": {
+         "color": "orange",
+        "weight": 2,
+      },
+    }).addTo(map);
+    
+    
+   
     $.each(coords, function(j, coord){
       var icon = L.MakiMarkers.icon({icon: "marker", color: (function(){
-        if(ladenStatus[ships[i].data[j].LaidenState]){
-          return '#'+ladenStatus[ships[i].data[j].LaidenState]
+        if(ship.LadenStatuses[j]){
+          return '#'+ladenStatus[ship.LadenStatuses[j]]
         }
         return '#00b'
       })(), size: "s"});
-      L.rotatedMarker([coord[1],coord[0]], {
+      
+      var marker = L.rotatedMarker([coord[1],coord[0]], {
         icon: icon,
-        angle: +ships[i].data[j].Heading+180 || 180
-      }).addTo(map);
-
-    //   L.Marker(new L.LatLng(37.9, -77), {
-    //     icon: L.icon({
-    //       "iconUrl": 'https://www.mapbox.com/maki/renders/marker-12@2x.png',
-    //       "iconSize": [12, 12],
-    //     }),
-    //     "angle": ships[i].data[j].Heading,
-    //     "style": {
-    //        "color": ladenStatus[ships[i].data[j].LaidenState],
-    //       "weight": 2,
-    //   },
-    // }).addTo(map);
+        angle: ship.Headings[j]+180 || 180
+      });
+      
+      markerList.push(marker);
     });
-  });
+    historyMarkers = L.featureGroup(markerList).addTo(map);
+    
+   // var bounds = L.latLngBounds(markerList);
+    map.fitBounds(historyMarkers.getBounds());
+        
 }  
 
 function makeRegions(rows){
@@ -115,27 +186,10 @@ function makeRegions(rows){
         },
       }).addTo(map);
   });
-  // var regions = _.uniq(_.pluck(rows, 'region'));
-  // $.each(regions, function(i, region){
-  //   var coords = [];
-  //   regions[i] = {'name': region, 'data': _.where(rows, {region:region})};
-  //   $.each(regions[i].data, function(i, obj){
-  //     coords.push([+obj.latitude, +obj.longitude]);
-  //   });
-  //   regions[i].coordinates = coords;
-  //   L.geoJson(polygon(coords),{
-  //     "style": {
-  //        "color": "blue",
-  //       "weight": .5,
-  //       "opacity": 0.05
-  //     },
-  //   }).addTo(map);
-  // });
 }
 
 function makePorts(rows){
   $.each(rows, function(i, port){
-    console.log(port)
     var coords = [];
     $.each(port.Points, function(i, obj){
         coords.push([+obj.Longitude, +obj.Latitude]);
@@ -149,3 +203,7 @@ function makePorts(rows){
       }).addTo(map);
   });
 }
+
+
+
+  
